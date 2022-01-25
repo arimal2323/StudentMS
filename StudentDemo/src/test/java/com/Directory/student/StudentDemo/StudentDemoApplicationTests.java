77a181324950786f4aa877b3d42cf9c7edf11642
Apurba RImal <84208480+arimal2323@users.noreply.github.com>
2022-01-25 12:16:48 -0600

    Add files via upload

---
+++ StudentDemo/src/test/java/com/Directory/student/StudentDemo/StudentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Directory.student.StudentDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
